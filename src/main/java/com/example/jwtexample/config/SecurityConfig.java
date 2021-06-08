d058e6f507cb5d8b881e97e00ace027a7096be38
DESKTOP-JBKN2TG\mjkim <mjkim@gaia3d.com>
2021-06-08 14:09:00 +0900

    basic controller 생성

---
+++ src/main/java/com/example/jwtexample/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.jwtexample.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity //기본적인 Web보안을 활성화 하겠다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override 
+    protected void configure(HttpSecurity http)throws Exception {
+        http.authorizeRequests() //http servlet request 를 사용하는 요청에 대해서 접근 제한을 하겠다.
+            .antMatchers("/api/hello").permitAll()
+            .anyRequest().authenticated(); //나머지 요청들은 모두 인증이 필요하다.
+    }
+
+
+    
+}
